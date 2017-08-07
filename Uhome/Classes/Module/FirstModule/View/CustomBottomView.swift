1e7b9f8cf24544fb3676af57e299b238da309610
程荣刚 <rong-gang.cheng@utsoft.cn>
2017-08-07 20:59:23 +0800

    添加自定义视图

---
+++ Uhome/Classes/Module/FirstModule/View/CustomBottomView.swift
@@ -0,0 +1,13 @@
+//
+//  CustomBottomView.swift
+//  Uhome
+//
+//  Created by 程荣刚 on 2017/8/7.
+//  Copyright © 2017年 menhao. All rights reserved.
+//
+
+import UIKit
+
+class CustomBottomView: UIView {
+
+}
